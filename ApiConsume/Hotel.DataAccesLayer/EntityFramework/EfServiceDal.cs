34850c574139d601243faa4935434c2e53480ba8
m-boga <muhammet.boga@hotmail.com>
2023-08-12 23:21:00 +0300

    Business Layer Created and Edited. Data Access Layer Edited.

---
+++ ApiConsume/Hotel.DataAccesLayer/EntityFramework/EfServiceDal.cs
@@ -0,0 +1,19 @@
+﻿using Hotel.DataAccesLayer.Abstract;
+using Hotel.DataAccesLayer.Concrete;
+using Hotel.DataAccesLayer.Repositories;
+using Hotel.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.DataAccesLayer.EntityFramework
+{
+    public class EfServiceDal : GenericRepository<Service>, IServiceDal
+    {
+        public EfServiceDal(Context context) : base(context)
+        {
+        }
+    }
+}
---
+++ ApiConsume/Hotel.DataAccesLayer/EntityFramework/EfSubscribeDal.cs
@@ -0,0 +1,19 @@
+﻿using Hotel.DataAccesLayer.Abstract;
+using Hotel.DataAccesLayer.Concrete;
+using Hotel.DataAccesLayer.Repositories;
+using Hotel.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.DataAccesLayer.EntityFramework
+{
+    public class EfSubscribeDal : GenericRepository<Subscribe>, ISubscribeDal
+    {
+        public EfSubscribeDal(Context context) : base(context)
+        {
+        }
+    }
+}
